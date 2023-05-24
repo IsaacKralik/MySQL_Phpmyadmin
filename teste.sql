0ee73c768dbd7f01498b9bc628aebff2b070134f
Isaac Kralik <104790651+IsaacKralik@users.noreply.github.com>
2023-05-24 12:20:39 -0300

    Teste

---
+++ teste.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO clientes (nome, email, empresa) VALUES ('Júnior', 'junior@dominio.com', 'RL System');
+INSERT INTO clientes (nome, email, empresa) VALUES ('Joao', 'joao@dominio.com', 'car system');
+INSERT INTO clientes (nome, email, empresa) VALUES ('alex', 'alex@dominio.com', 'RL System');
+INSERT INTO clientes (nome, email, empresa) VALUES ('pedro', 'pedro@dominio.com', 'car system');
+INSERT INTO clientes (nome, email, empresa) VALUES ('marcio', 'marcio@dominio.com', 'RL System');
+INSERT INTO clientes (nome, email, empresa) VALUES ('costa', 'costa@dominio.com', 'car system');
+INSERT INTO clientes (nome, email, empresa) VALUES ('jhon', 'jhon@dominio.com', 'RL System');
+INSERT INTO clientes (nome, email, empresa) VALUES ('pedro', 'pedro@dominio.com', 'car system');
+UPDATE clientes SET nome = 'Rafael', email = 'contato@rlsystem.com.br';
+DELETE FROM clientes WHERE id = 6;
+
+
+
+
